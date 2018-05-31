71cc13ee4f07ca0cf7602ef21a7c161e60929ecb
Matthieu Coudron <mattator@gmail.com>
2018-05-31 17:58:10 +0900

    addressing reviews

--- nixos/modules/programs/mininet.nix
+++ nixos/modules/programs/mininet.nix
@@ -8,6 +8,22 @@ let
   cfg  = config.programs.mininet;
 
   pyEnv = pkgs.python.withPackages(ps: [ ps.mininet-python ]);
+
+
+  path = with pkgs; makeSearchPath "bin"  [
+      iperf
+      telnet # mn will stop if no telnet is available
+      ethtool iproute socat ];
+
+  wrappedMnexec = pkgs.runCommand "mnexec-wrapper"
+    { buildInputs = [ pkgs.makeWrapper ]; }
+    ''
+      # This wrapper prevents from polluting PATH
+      makeWrapper ${pkgs.mnexec}/bin/mnexec \
+        $out/bin/mnexec \
+        --prefix PATH : "${generatedPath}"
+
+    '';
 in
 {
   options.programs.mininet.enable = mkEnableOption ''
@@ -18,11 +34,12 @@ in
 
     virtualisation.vswitch.enable = true;
 
-    environment.systemPackages = with pkgs; [
-      iperf
-      telnet # mn will stop if no telnet is available
-      ethtool iproute socat
-    ];
+    # environment.systemPackages = with pkgs; [
+    #   wrappedMnexec
+    #   iperf
+    #   telnet # mn will stop if no telnet is available
+    #   ethtool iproute socat
+    # ];
 
     security.wrappers = {
       mnexec.source = "${pkgs.mininet}/bin/mnexec";
