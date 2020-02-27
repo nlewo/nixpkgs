b0a0a9d7b28cf5f6b3e6e24bfa9215609da26459
Antoine Eiche <lewo@abesis.fr>
2020-02-27 18:00:59 +0100

    Update db2rst

--- nixos/doc/manual/rest.nix
+++ nixos/doc/manual/rest.nix
@@ -9,8 +9,8 @@ rec {
       src = pkgs.fetchFromGitHub {
         owner = "nlewo";
         repo = name;
-        rev = "edb83decc2d5806265da5ce6bdebc72e7ae78871";
-        sha256 = "1nyri71c6frp0cx369pydvs2b8h14lczgmx1nqrkdyzp3l6rza30";
+        rev = "e9e3d0aa3d4c4ae383cdc33deb14d3e10fd19155";
+        sha256 = "14yxjy7vzpm3qq2i4fvhknzkgw8simr1m7iv9rimliwan0hpm863";
       };
       buildInputs = [ (pkgs.python2.withPackages(p: [p.lxml])) ];
       installPhase = ''
@@ -27,9 +27,12 @@ rec {
   }
   ''
     mkdir -p $out/generated
+
+    # For Dev mode
     # There are about 10 000 options and we selected a subset to speedup error and try :/
     # xmlstarlet ed -N "x=http://docbook.org/ns/docbook"  -d 'x:appendix/x:variablelist/x:varlistentry[position()>100]' ${generatedSources}/options-db.xml > $out/generated/options-db.xml
-    # At the end, we still need to format the xml file
+
+    # We still need to format the xml file in order to convert it properly:/
     xmlstarlet fo ${generatedSources}/options-db.xml > $out/generated/options-db.xml 
     db2rst $out/generated/options-db.xml > $out/generated/options-db.xml.rst
   '';
@@ -46,6 +49,7 @@ rec {
       -not -name "options-db" \
       -exec bash -c "db2rst '{}' > '{}.rst'" \;
 
+    rsync -av --include '**/' --include '*.xml' --exclude '*' ./ $out/
     rsync -av --include '**/' --include '*.rst' --exclude '*' ./ $out/
     cp ${./index.rst} $out/index.rst
     cp ${./conf.py} $out/conf.py
